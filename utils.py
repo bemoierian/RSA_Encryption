76a0be616781281b3a1a2d15297f4b74c42ad2f5
Bemoi Erian <bemoi.erian@gmail.com>
2023-03-23 20:43:00 +0200

    generate random primes given number of bits

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import re
+import random
+
 class Utils:
     @staticmethod
     def replaceText(text):
@@ -134,3 +136,37 @@ class Utils:
         if t < 0:
             t += n
         return t
+    
+    @staticmethod    
+    def generate_big_prime(size):
+        p = random.randrange(2 ** (size - 1), 2 ** size - 1)
+        while not Utils.is_prime(p):
+            p = random.randrange(2 ** (size - 1), 2 ** size - 1)
+        return p
+    
+    @staticmethod
+    def is_prime(n, k=128):
+        if n == 2 or n == 3:
+            return True
+        if n <= 1 or n % 2 == 0:
+            return False
+
+        s = 0
+        r = n - 1
+        while r & 1 == 0:
+            s += 1
+            r //= 2
+
+        for _ in range(k):
+            a = random.randrange(2, n - 1)
+            x = pow(a, r, n)
+            if x != 1 and x != n - 1:
+                j = 1
+                while j < s and x != n - 1:
+                    x = pow(x, 2, n)
+                    if x == 1:
+                        return False
+                    j += 1
+                if x != n - 1:
+                    return False
+        return True
