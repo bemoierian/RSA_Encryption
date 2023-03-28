b9a1ba0c8b1e4aee8f2a4b4070243d34fe925d45
Bemoi Erian <bemoi.erian@gmail.com>
2023-03-28 22:39:33 +0200

    improve attack

--- attack.py
+++ attack.py
@@ -4,22 +4,36 @@ import time
 # import matplotlib.pyplot as plt
 import multiprocessing
 
+# def attack(cypherText, plaintext, n, d, result, index):
+#     # global stop_threads
+#     stopValue = (index+1)*(n//threadNum)
+#     # d = 0
+#     decyptedMsg = Utils.decryptMessage(cypherText, d, n)
+#     decodedMsg = Utils.decodeMessage(decyptedMsg)
+#     # print(decodedMsg)
+#     # print("Breaking key...")
+#     while decodedMsg.strip() != plaintext:
+#         d = d + 1
+#         decyptedMsg = Utils.decryptMessage(cypherText, d, n)
+#         decodedMsg = Utils.decodeMessage(decyptedMsg)
+#         if d % 100000 == 0:
+#             print(f"Process {index}: d = " + str(d))
+#         # print(decodedMsg)
+#         # if stop_threads:
+#         #     return -1
+#         if d >= stopValue:
+#             print(f"Process {index}: Exiting...")
+#             return 0
+#     # stop_threads = True
+#     print(f"Process {index}: Key broken successfully")
+#     result[index] = d
+#     return d
 
-def search_list(value, lst):
-    for i in range(len(lst)):
-        if lst[i] == value:
-            print(
-                f"Found {value} at index {i} in {threading.current_thread().name}")
-            for t in threading.enumerate():
-                if t != threading.current_thread():
-                    t.kill()
-            return i
-    return -1
 
-
-def attack(cypherText, plaintext, n, d, result, index):
+def attack(cypherText, plaintext, n, d, threadIndex, stopValue):
+    print(f"Starting process {threadIndex}... start index is {d}")
     # global stop_threads
-    stopValue = (index+1)*(n//threadNum)
+    # stopValue = (threadIndex+1)*(n//threadNum)
     # d = 0
     decyptedMsg = Utils.decryptMessage(cypherText, d, n)
     decodedMsg = Utils.decodeMessage(decyptedMsg)
@@ -30,35 +44,38 @@ def attack(cypherText, plaintext, n, d, result, index):
         decyptedMsg = Utils.decryptMessage(cypherText, d, n)
         decodedMsg = Utils.decodeMessage(decyptedMsg)
         if d % 100000 == 0:
-            print(f"Process {index}: d = " + str(d))
+            print(f"Process {threadIndex}: d = " + str(d))
         # print(decodedMsg)
         # if stop_threads:
         #     return -1
         if d >= stopValue:
-            print(f"Process {index}: Exiting...")
+            print(f"Process {threadIndex}: Exiting...")
             return 0
     # stop_threads = True
-    print(f"Process {index}: Key broken successfully")
-    result[index] = d
+    print(f"Process {threadIndex}: Key broken successfully")
+    # result[threadIndex] = d
     return d
 
 
-plaintext = "hello world"
-# -----------------14 bits-----------------
-# cypherText = "20923090 26619114 69194545".split(
-#     " ")
-# d = 6411641
-# n = 189467017
-# -----------------16 bits-----------------
-cypherText = "741850173 1876460621 725561250".split(
-    " ")
-# d = 6940807
-n = 1880353231
-threadNum = 6
-results = [None] * threadNum
-threads = [None] * threadNum
 # stop_threads = False
 if __name__ == '__main__':
+    print("Starting attack...")
+    nBits = 32
+    print(f"nbits = {nBits}")
+    # cypherText = "871998463 1613204058 1941644450 1399719919 2491211954 1816385893 1831371137".split(" ")
+    p = Utils.generate_big_prime(nBits)
+    q = Utils.generate_big_prime(nBits)
+    n = p * q
+    phiN = (p - 1) * (q - 1)
+    e = Utils.generate_e(phiN)
+    print("n = " + str(n))
+    plaintext = "secret"
+    encoddedMsg = Utils.encodeMessage(plaintext)
+    cypherText = Utils.encryptMessage(encoddedMsg, e, n)
+    # threadNum = 6
+    processesNum = 6
+    reald = Utils.modInverse(e, phiN)
+    print("Real d = " + str(reald))
     # -----------------multithreading-----------------
     # start = time.time()
     # for i in range(threadNum):
@@ -76,9 +93,9 @@ if __name__ == '__main__':
 
     # -----------------multiprocessing-----------------
     start = time.time()
-    with multiprocessing.Pool(processes=threadNum) as pool:
-        results2 = [pool.apply_async(attack, (cypherText, plaintext, n, i*(n//threadNum), results, i))
-                    for i in range(threadNum)]
+    with multiprocessing.Pool(processes=processesNum) as pool:
+        results2 = [pool.apply_async(attack, (cypherText, plaintext, n, i*(n//processesNum),  i, (i+1)*(n//processesNum)))
+                    for i in range(processesNum)]
         for r in results2:
             if r.get() != 0:
                 print("d = " + str(r.get()))
@@ -88,7 +105,7 @@ if __name__ == '__main__':
 
     # -------------------------------------------------
     timeTaken = end - start
-    print("14 bits | Time taken: " + str(timeTaken) + " seconds")
+    print(f"{nBits} bits | Time taken: " + str(timeTaken) + " seconds")
 
     x = [14, 16, 32, 64, 128, 256, 512, 1024]
     y = [111, 153]
